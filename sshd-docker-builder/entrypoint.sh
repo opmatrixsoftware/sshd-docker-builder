00493db498990ab429a86c2eed28b94b86c57cc9
A. Kevin Bailey <akevinbailey@yahoo.com>
2019-06-01 17:04:59 +0200

    version 0.3

--- sshd-docker-builder/entrypoint.sh
+++ sshd-docker-builder/entrypoint.sh
@@ -9,5 +9,9 @@ touch /root/.ssh/authorized_keys
 echo ${PUBLIC_KEY} > /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 
+echo 1 > /proc/sys/net/ipv4/ip_forward
+modprobe br_netfilter
+sysctl --system
+
 /usr/bin/dockerd &
 /usr/sbin/sshd -D

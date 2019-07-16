1551db31a0a3ccdafc224666babf2aa20a0303a1
A. Kevin Bailey <akevinbailey@yahoo.com>
2019-07-16 11:41:09 -0500

    version 0.6

--- sshd-docker-builder/entrypoint.sh
+++ sshd-docker-builder/entrypoint.sh
@@ -4,13 +4,12 @@ ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
 ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
 
-if [[ -n "$PUBLIC_KEY"  ]]; then
-    mkdir -p /root/.ssh
-    touch /root/.ssh/authorized_keys
-    echo ${PUBLIC_KEY} > /root/.ssh/authorized_keys
-    unset ${PUBLIC_KEY}
-    chmod 600 /root/.ssh/authorized_keys
-fi
+mkdir -p /root/.ssh
+touch /root/.ssh/authorized_keys
+echo ${SSH_PUB_KEY} > /root/.ssh/authorized_keys
+unset ${SSH_PUB_KEY}
+chmod 600 /root/.ssh/authorized_keys
+
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 modprobe br_netfilter

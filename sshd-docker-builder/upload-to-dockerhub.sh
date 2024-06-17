7949abae0720b0a3a9f527dd4d62f01e74394ed4
akbailey <akevinbailey@yahoo.com>
2024-06-16 22:06:59 -0500

    2.3

--- sshd-docker-builder/upload-to-dockerhub.sh
+++ sshd-docker-builder/upload-to-dockerhub.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Need to add the Docker Hub account name before the image name to push to Docker Hub.
 docker image tag d583c3ac45fd opmatrix/sshd-docker-builder:9.4
+docker image tag d583c3ac45fd opmatrix/sshd-docker-builder
 docker push opmatrix/sshd-docker-builder:9.4
+docker push opmatrix/sshd-docker-builder
